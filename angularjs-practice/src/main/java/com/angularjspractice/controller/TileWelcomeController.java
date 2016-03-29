ab5c0f7aa1912b06a148aaac7b2b8fbf3b34042e
joe <joeldyscs@gmail.com>
2016-03-29 19:41:46 -0400

    changes added to the project to make it work with tiles. Working fine only tiles. Angular requests don't work

---
+++ angularjs-practice/src/main/java/com/angularjspractice/controller/TileWelcomeController.java
@@ -0,0 +1,21 @@
+package com.angularjspractice.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TileWelcomeController {
+
+	@RequestMapping("/welcome")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", "Hello Spring Boot with Tiles");
+		return "welcome";
+	}
+	
+	@RequestMapping("/")
+	public String index(Map<String, Object> model) {
+		return "index";
+	}
+}
