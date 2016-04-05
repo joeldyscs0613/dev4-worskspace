8c77f138bd4fbced84872d05fc12179d8457a691
joe <joeldyscs@gmail.com>
2016-04-04 20:40:03 -0400

    gravatarUrlBuilder.js added

---
+++ angularjs-demoapp/src/main/resources/public/js/controllers/editProfileController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+eventsApp.controller('editProfileController',
+    function editProfileController($scope, gravatarUrlBuilder) {
+        $scope.user = {};
+
+        $scope.getGravatarUrl = function(email) {
+        	return gravatarUrlBuilder.buildGravatarUrl(email);
+        }
+
+    }
+);
