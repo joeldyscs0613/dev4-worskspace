514a9841019bbe6e22fc801db229dbc5b0cf575f
joe <joeldyscs@gmail.com>
2016-04-06 21:03:30 -0400

    changed index location and added configuration prefix

--- angularjs-demoapp/src/main/java/com/demoapp/controller/HomeController.java
+++ angularjs-demoapp/src/main/java/com/demoapp/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.demoapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Welcome Home!";
+		return "index.html";
 	}
 }
--- angularjs-demoapp/src/main/resources/public/js/app.js
+++ angularjs-demoapp/src/main/resources/public/js/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource']);
-
-
-/*controller('ShipwreckEditController', function($scope, $state, $stateParams, Shipwreck) {
-	  $scope.updateShipwreck = function() { //Update the edited shipwreck. Issues a PUT to /api/v1/shipwrecks/:id
-	    $scope.shipwreck.$update(function() {
-	      $state.go('shipwrecks'); // on success go back to the list i.e. shipwrecks state.
-	    });
-	  };*/
+var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource', 'ngRoute'])
+				.config(function ($routeProvider) {
+					$routeProvider
+					.when('/newEvent', 
+						{
+							templateUrl : 'newEvent.html', 
+							controller : 'editEventController', 
+						})
+				});
